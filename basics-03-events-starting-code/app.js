9b5ec1814b2d48be958caa18061c336d630a028d
shivam sharma <shivamsharma38391@gmail.com>
2023-04-05 14:25:42 +0530

    input element with function

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,11 +2,19 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: ""
     };
   },
   methods: {
     // dynamic function 
     // passing arguments
+    // new
+    setname(event) {
+      this.name = event.target.value;
+      console.log(event.target)
+
+
+    },
     addcounter(num) {
       this.counter = this.counter + num;
     },
